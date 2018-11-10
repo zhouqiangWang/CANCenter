e9ecc8239777bb1bf0ad778ec937c2ace70f0ef3
ITU Test server 90 <itutest@itu.edu>
2018-11-09 18:23:58 -0800

    add libraries and ffi call method

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const ffi = require("ffi");
+const canLib = ffi.Library("./lib/linux/64bit/libGinkgo_Driver.so", {
+  'VCI_ScanDevice': ['int', ['int']]
+});
+
+let deviceN = canLib.VCI_ScanDevice(1);
+console.log("deviceN = ", deviceN);
 console.log("hello from CAN");
